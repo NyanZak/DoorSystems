62b4a48a37beced503962c18637dd5a5eeb0d838
Zak Cronin <71699711+NyanZak@users.noreply.github.com>
2022-03-08 12:04:50 +0000

    Add files via upload

---
+++ Assets/Components/DoorKeySystem/Scripts/DoorTriggerPressurePlate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTriggerPressurePlate : MonoBehaviour {
+
+    [SerializeField] private DoorAnimated door;
+
+    private float timer;
+
+    private void Update() {
+        if (timer > 0f) {
+            timer -= Time.deltaTime;
+            if (timer <= 0f) {
+                door.CloseDoor();
+            }
+        }
+    }
+    private void OnTriggerStay(Collider collision) {
+        float timeToStayOpen = 2f;
+        timer = timeToStayOpen;
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        door.OpenDoor();
+    }
+
+}
